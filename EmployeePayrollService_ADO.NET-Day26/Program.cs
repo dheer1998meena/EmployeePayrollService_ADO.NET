7ad1d7fe62ebf9cb4a55c560a3104b17f732875c
dheer meena <dheer1998meena@gmail.com>
2020-11-11 01:24:10 +0530

    [Dheer] Add: Implemented the query to get the basic pay statitics for a particular gender after grouping

--- EmployeePayrollService_ADO.NET-Day26/Program.cs
+++ EmployeePayrollService_ADO.NET-Day26/Program.cs
@@ -21,6 +21,9 @@ namespace EmployeePayrollService_ADO.NET_Day26
             Console.WriteLine(repository.UpdateSalaryIntoDatabase("Terissa", 50000) ? "Update done successfully " : "Update Failed");
             /// UC5 Gets the employees details for a particular date range.
             repository.GetEmployeesFromForDateRange("2018 - 05 - 03");
+            /// UC6 Getting the detail of salary ofthe employee joining grouped by gender and searched for a particular gender.
+            repository.FindGroupedByGenderData("M");
+            Console.ReadKey();
         }
     }
 }
