05155ad22e0b7b03f6a8510919a8feac62a67d3e
dheer meena <dheer1998meena@gmail.com>
2020-11-11 12:52:17 +0530

    [Dheer] Add: Retrieving all the employee details from multible tables after implementing E-R concept

--- EmployeePayrollService_ADO.NET-Day26/Program.cs
+++ EmployeePayrollService_ADO.NET-Day26/Program.cs
@@ -12,20 +12,22 @@ namespace EmployeePayrollService_ADO.NET_Day26
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Payroll Services Using ADO.NET");
-            ///Creating a instance object of EmployeeRepository class.
-            EmployeeRepository repository = new EmployeeRepository();
-            ///UC1 Creating a method for checking for the validity of the connection.
-            repository.EnsureDataBaseConnection();
-            ///UC3 Updates the given empname with given salary into database.
-            Console.WriteLine(repository.UpdateSalaryIntoDatabase("Terissa", 50000) ? "Update done successfully " : "Update Failed");
-            /// UC5 Gets the employees details for a particular date range.
-            repository.GetEmployeesFromForDateRange("2018 - 05 - 03");
-            /// UC6 Getting the detail of salary ofthe employee joining grouped by gender and searched for a particular gender.
-            repository.FindGroupedByGenderData("M");
-            Console.ReadKey();
-            /// UC7 Inserts data into multiple tables using transactions.
-            repository.InsertIntoMultipleTablesWithTransactions();
+            //Console.WriteLine("Welcome to Employee Payroll Services Using ADO.NET");
+            /////Creating a instance object of EmployeeRepository class.
+              EmployeeRepository repository = new EmployeeRepository();
+            /////UC1 Creating a method for checking for the validity of the connection.
+            //repository.EnsureDataBaseConnection();
+            /////UC3 Updates the given empname with given salary into database.
+            //Console.WriteLine(repository.UpdateSalaryIntoDatabase("Terissa", 50000) ? "Update done successfully " : "Update Failed");
+            ///// UC5 Gets the employees details for a particular date range.
+            //repository.GetEmployeesFromForDateRange("2018 - 05 - 03");
+            ///// UC6 Getting the detail of salary ofthe employee joining grouped by gender and searched for a particular gender.
+            //repository.FindGroupedByGenderData("M");
+            //Console.ReadKey();
+            ///// UC7 Inserts data into multiple tables using transactions.
+            //repository.InsertIntoMultipleTablesWithTransactions();
+            ///UC8 Retrieves the employee details from multiple tables after implementing E - R concept.
+             repository.RetrieveEmployeeDetailsFromMultipleTables();
         }
     }
 }
